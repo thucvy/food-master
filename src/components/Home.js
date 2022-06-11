4e1eac04e092dc8bcd9ac6f6ba9b14d05692fed5
Nguyen <vy.nguyen@adastragrp.com>
2022-06-11 18:00:26 -0400

    implement home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,30 @@
-import { Container } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { GiPressureCooker } from "react-icons/gi";
 
 const Home = () => {
-  return <Container>- Intro/ Demo and Button to Get Started</Container>;
+  return (
+    <Container
+      style={{
+        position: "absolute",
+        top: "50%",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>Struggling with what to eat or cook?</h1>
+      <LinkContainer to="/fridge" style={{ width: "fit-content" }}>
+        <Nav.Link>
+          <Button variant="warning">
+            Let's cook with us!
+            <GiPressureCooker className="mx-1" />
+          </Button>
+        </Nav.Link>
+      </LinkContainer>
+    </Container>
+  );
 };
 
 export default Home;
